be70a5b3e86a004e15647adbb32f4e9b95cb1b00
= <xandr.rudkovsky@gmail.com>
2019-11-04 02:06:03 +0300

    C5. Add authorization method.

--- IEWorker.py
+++ IEWorker.py
@@ -3,7 +3,9 @@ from win32com.client import Dispatch
 
 
 class IEWorker:
-    def __init__(self, url=''):
+    use_auth = True
+
+    def __init__(self, url='', use_auth=True):
         if url:
             win_id = '{9BA05972-F6A8-11CF-A442-00A0C90A8F39}'
             win_shells = Dispatch(win_id)
@@ -17,10 +19,18 @@ class IEWorker:
         if url:
             self.navigate(url)
 
-    def navigate(self, url):
+    def _navigate(self, url):
         self.ie.Navigate(url)
         self.ie_busy()
         self._get_DOM()
+
+    def navigate(self, url):
+        self._navigate(url)
+        if self.check_auth():
+            self.auth()
+            self.ie_busy()
+        if url != self.auth_url and url != self.ie.LocationURL:
+            self._navigate(url)
 
     def ie_busy(self):
         while self.ie.Busy:
@@ -53,3 +63,34 @@ class IEWorker:
             elems = None
 
         return elems
+
+    def set_auth(self,
+                 auth_url='',
+                 auth_login_id='',
+                 auth_pwd_id='',
+                 auth_login='',
+                 auth_pwd='',
+                 auth_button='',
+                 ):
+        self.use_auth = True
+        self.auth_url = auth_url
+        self.auth_login_id = auth_login_id
+        self.auth_pwd_id = auth_pwd_id
+        self.auth_login = auth_login
+        self.auth_pwd = auth_pwd
+        self.auth_button = auth_button
+
+    def check_auth(self):
+        if self.ie.LocationURL == self.auth_url:
+            return True
+        else:
+            return False
+
+    def auth(self):
+        login = self.get_elements(id=self.auth_login_id)['elem']
+        pwd = self.get_elements(id=self.auth_pwd_id)['elem']
+        button = self.get_elements(id=self.auth_button)['elem']
+        login.setAttribute('value', self.auth_login)
+        pwd.setAttribute('value', self.auth_pwd)
+        button.click()
+
--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 from IEWorker import *
+from config import *
 
 
-ie = IEWorker('https://office.iml.ru')
-ie.close()
+ie = IEWorker()
+ie.set_auth(
+        auth_url=url_login_form,
+        auth_login_id=login_id,
+        auth_pwd_id=pwd_id,
+        auth_login=iml_login,
+        auth_pwd=iml_pwd,
+        auth_button='Button1',
+    )
+ie.navigate('https://office.iml.ru/')
+
+# ie.close()
