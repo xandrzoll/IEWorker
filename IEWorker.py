68c7f194f674cb3debdb9307dd9d70baeb0e2372
= <xandr.rudkovsky@gmail.com>
2019-11-04 00:47:55 +0300

    C2. Augmented init method

--- IEWorker.py
+++ IEWorker.py
@@ -2,7 +2,15 @@ from win32com.client import Dispatch
 
 
 class IEWorker:
-    def __init__(self):
+    def __init__(self, url):
+        if url:
+            win_id = '{9BA05972-F6A8-11CF-A442-00A0C90A8F39}'
+            win_shells = Dispatch(win_id)
+            url = url.lower()
+            for win_shell in win_shells:
+                if url in win_shell.LocationURL.lower():
+                    self.ie = win_shell
+                    return
         self.ie = Dispatch("InternetExplorer.Application")
         self.ie.Visible = True
 
