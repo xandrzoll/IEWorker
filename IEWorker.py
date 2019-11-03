9dde38c1e787868db98ed3c738f6fe73c40ae60b
unknown <xandr.rudkovsky@gmail.com>
2019-11-04 00:33:36 +0300

    C1. Add navigate and get_elements methods to IEWorker class

--- IEWorker.py
+++ IEWorker.py
@@ -6,3 +6,8 @@ class IEWorker:
         self.ie = Dispatch("InternetExplorer.Application")
         self.ie.Visible = True
 
+    def navigate(self, url):
+        self.ie.Navigate(url)
+
+    def get_elements(self, id='', name='', cls='', tag=''):
+        pass
